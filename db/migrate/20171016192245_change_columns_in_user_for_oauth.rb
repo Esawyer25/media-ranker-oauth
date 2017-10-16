f14f4abe201bb93729269c83d682260e76000d3d
Eva Sawyer <esawyer25@gmail.com>
2017-10-16 12:28:00 -0700

    inital Oauth setup

---
+++ db/migrate/20171016192245_change_columns_in_user_for_oauth.rb
@@ -0,0 +1,11 @@
+class ChangeColumnsInUserForOauth < ActiveRecord::Migration[5.0]
+  def change
+   add_column :users, :name, :string
+   add_column :users, :email, :string
+   add_column :users, :provider, :string, null: false
+   add_column :users, :uid, :string, null: false
+  end
+end
+
+
+#name:string username:string email:string provider:string uid:string
